9648a2ad18f73c750ab99a9666570bd85941c3ac
ChihYuChiang <ckccyu@gmail.com>
2018-10-26 23:24:04 +0800

    Add: store, json operation

--- bin/ImgDownloader.py
+++ bin/ImgDownloader.py
@@ -1,21 +1,56 @@
 import requests
-import yaml
 import pandas as pd
 import pickle
+import sys
 from pprint import pprint
 from bidict import bidict
 import bin.module.util as util
 
 
-#--Initialize data container
-data = util.UniversalContainer()
+#--Initialize data storage
+#Also create `session` attribute in the class
+class Store():
+
+    #Initialize store content
+    def initSession(path):
+        try:
+            with open(path, 'rb') as f: return pickle.load(f, encoding='utf-8')
+        except:
+            session = util.UniversalContainer()
+            session.currentId = 0
+            return session
+
+    config = util.SettingContainer(
+        cred=util.getConfigObj('ref/credential.yml'),
+        sessionPath='data/session.pkl'
+    )
+    data = util.UniversalContainer()
+    session = initSession(config.sessionPath)
+
+    @classmethod
+    def dumpSession(cls, path):
+        with open(path, 'wb') as f: pickle.dump(cls.session, f)
+    
+    @classmethod
+    def loadSession(cls, path):
+        with open(path, 'rb') as f: cls.session = pickle.load(f, encoding='utf-8')    
+
+    #Show all store content
+    @classmethod
+    def reveal(cls):
+        exp = '<session>\n{}\n\n<config>\n{}\n\n<data>\n{}'.format(cls.session, cls.config, cls.data)
+        print(exp)
+
+#Store session info offline
+Store.dumpSession(Store.config.sessionPath)
 
 
 #--Prepare name-id 2-way mapping
 class Mapping():
 
-    def generate(): #Process from source data
-        df = pd.read_csv('data/text/df_cb_main_combined.csv', usecols=['Game']).drop_duplicates().reset_index(drop=True)
+    #Process from source data
+    def generate():
+        df = pd.read_csv('data/text/df_cb_main_combined.csv', usecols=['Game'], keep_default_na=False).drop_duplicates().reset_index(drop=True)
         return bidict(df.to_dict()['Game'])
 
     @classmethod
@@ -30,18 +65,24 @@ class Mapping():
         print(len(mapping))
 
 #Mapping data
-data.mapping = Mapping.generate()
+Store.data.mapping = Mapping.generate()
 
 
-#--Acquire img paths (Bing)
+#--Acquire img urls (Bing)
 class ImgSearch():
 
     #Bing setup
-    CRED = util.getConfigObj('ref/credential.yml')
+    #https://docs.microsoft.com/en-us/rest/api/cognitiveservices/bing-images-api-v7-reference
     setting = util.SettingContainer(
-        url=CRED.BingImageSearch.url,
-        headers={'Ocp-Apim-Subscription-Key' : CRED.BingImageSearch.key},
-        params={'q': '', 'license': 'public', 'imageType': 'photo'}
+        url=Store.config.cred.BingImageSearch.url,
+        headers={
+            'Ocp-Apim-Subscription-Key': Store.config.cred.BingImageSearch.key
+        },
+        params={
+            'q': '', 'license': 'all', 'imageType': 'photo',
+            'count': 100, 'safeSearch': 'off',
+            'maxFileSize': 520192, 'minFileSize':0 #byte
+        }
     )
 
     @classmethod
@@ -52,21 +93,58 @@ class ImgSearch():
         return response.json()
     
     @classmethod
+    def searchMappingBatch(cls, mapping, startId, currentResponses=[]):
+        ids = list(mapping)[startId:5]
+        idIter = iter(ids)
+        responses = currentResponses
+
+        while True:
+            try:
+                targetId = next(idIter)
+                targetTerm = '{} game'.format(Store.data.mapping[targetId]) 
+                response = cls.search(targetTerm)
+                response['targetId'] = targetId
+                responses.append(response)
+            except StopIteration: return responses, targetId + 1
+            except:
+                print("Unexpected error:", sys.exc_info()[0])
+                return responses, targetId
+    
+    @classmethod
     def test(cls):
+        #Check Append Results for Jupyter to render
         response = cls.search('puppies')
         pprint(response)
+    
+    def parseResponse(response):
+        urls = 1
+        return response['targetId'], urls
+    
+    @classmethod
+    def parseResponses(cls, responses):
+        urlInfo = []
+        for response in responses:
+            urlInfo.append(cls.parseResponse(response))
+        return urlInfo
 
+Store.data.responses, Store.session.currentId = ImgSearch.searchMappingBatch(Store.data.mapping, Store.session.currentId)
+utils.writeJsls(Store.data.responses, 'data/image-response.jsl')
 
-for i in mapping.inv: print(i)
+Store.data.responses = utils.readJsls('data/image-response.jsl')
+len(Store.data.responses)
+test[4]
 
+Store.data.urlInfo = ImgSearch.parseResponses(Store.data.responses)
 
 
 #--Download img
-img_url = 'http://images.fineartamerica.com/images-medium-large-5/abstract-art-original-painting-winter-cold-by-madart-megan-duncanson.jpg'
-path = 'data/img/img_url1.jpeg'
+class ImgDownload():
 
-r = requests.get(img_url, stream=True)
-if r.status_code == 200:
-    with open(path, 'wb') as f:
-        for chunk in r.iter_content(1024): #'1024 = chunk size
-            f.write(chunk)
+    img_url = 'http://images.fineartamerica.com/images-medium-large-5/abstract-art-original-painting-winter-cold-by-madart-megan-duncanson.jpg'
+    path = 'data/img/img_url1.jpeg'
+
+    r = requests.get(img_url, stream=True)
+    if r.status_code == 200:
+        with open(path, 'wb') as f:
+            for chunk in r.iter_content(1024): #'1024 = chunk size
+                f.write(chunk)
--- bin/module/util.py
+++ bin/module/util.py
@@ -1,3 +1,59 @@
+#--Decorators for classes
+class ClsDecorator():
+
+    def prohibitAttrSetter(cls):
+        """
+        Prohibit access to attribute setter
+        """
+        def setattr(self, key, value):
+            class ProhibittedOperation(Exception): pass
+            raise ProhibittedOperation('Not allowed to modify attributes directly.')
+
+        cls.__setattr__ =  setattr
+        return cls
+    
+    def grantKeywordUpdate(cls):
+        """
+        Grant attribute modification by `update` method
+        """
+        def update(self, **kwarg):
+            for key, value in kwarg.items():
+                self.__dict__[key] = value
+
+        cls.update = update
+        cls.__init__ = update        
+        return cls
+
+    #Isn't in use. However, a good sample of decorator with arg input
+    def grantSessionStorage(path):
+        """
+        Grant methods dealing with offline session info storage
+        - Use store `session` attribute.
+        """
+        def wrapper(cls):
+            def initSession(path=path):
+                try:
+                    with open(path, 'rb') as f: return pickle.load(f, encoding='utf-8')
+                except:
+                    return util.UniversalContainer()
+
+            @classmethod
+            def dumpSession(cls, path=path):
+                with open(path, 'wb') as f: pickle.dump(cls.session, f)
+            
+            @classmethod
+            def loadSession(cls, path=path):
+                with open(path, 'rb') as f: cls.session = pickle.load(f, encoding='utf-8')
+            
+            cls.session = initSession()
+            cls.initSession = initSession
+            cls.dumpSession = dumpSession      
+            cls.loadSession = loadSession
+            
+            return cls
+        return wrapper
+
+
 class UniversalContainer():
     """
     Usage
@@ -28,9 +84,21 @@ class UniversalContainer():
         return [item for item in dir(self) if callable(getattr(self, item)) and not item.startswith("__")]
 
 
-class ConvertedContainer(UniversalContainer):
+@ClsDecorator.prohibitAttrSetter
+@ClsDecorator.grantKeywordUpdate
+class SettingContainer(UniversalContainer):
     """
-    Convert data to object form (recursive).
+    Usage
+    - Convenient keyword = parameter setting.
+    - Protected attribute setter. Use `update(key=value)` to modify content.
+    """
+    pass
+   
+
+class ConvertedContainer(SettingContainer):
+    """
+    Usage
+    - Convert dict to object form (recursive).
     """
     
     def __new__(cls, data):
@@ -45,26 +113,7 @@ class ConvertedContainer(UniversalContainer):
 
     def __init__(self, data):
         for i in range(len(data.keys())):
-            setattr(self, list(data.keys())[i], ConvertedContainer(list(data.values())[i]))
-
-
-class SettingContainer(UniversalContainer):
-    """
-    Usage
-    - Convenient keyword = parameter setting.
-    - Protected attribute setter. Use `update(key=value)` to modify content.
-    """
-
-    def __setattr__(self, key, value):
-        class ProhibittedOperation(Exception): pass
-        raise self.ProhibittedOperation('Use SettingContainer.update to modify attributes.')
-
-    def update(self, **kwarg):
-        for key, value in kwarg.items():
-            self.__dict__[key] = value
-            # setattr(self, key, value)
-
-    __init__ = update
+            self.__dict__[list(data.keys())[i]] = ConvertedContainer(list(data.values())[i])
 
 
 def getConfigObj(path):
@@ -84,3 +133,34 @@ def getConfigObj(path):
         else: raise UnknownFileType('\'{}\' is not a supported file type.'.format(ext))
 
     return ConvertedContainer(config_dic)
+
+
+def writeJsls(obj, path):
+    import json
+    import numpy
+
+    #Deal with the json default encoder defect
+    #https://stackoverflow.com/questions/27050108/convert-numpy-type-to-python/27050186#27050186
+    class NumpyEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, numpy.integer):
+                return int(obj)
+            elif isinstance(obj, numpy.floating):
+                return float(obj)
+            elif isinstance(obj, numpy.ndarray):
+                return obj.tolist()
+            else:
+                return super(NumpyEncoder, self).default(obj)
+            
+    with open(path, mode='a') as f:
+        for item in obj:
+            json.dump(item, f, cls=NumpyEncoder)
+            f.write('\n')
+
+
+def readJsls(path):
+    output = []
+    with open(path, mode='r') as f:
+        for line in f:
+            output.append(json.loads(line))
+    return output
