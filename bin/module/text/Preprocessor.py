b0fb612d3678bd24f26ff5c728e3db72970dbbef
ChihYuChiang <ckccyu@gmail.com>
2019-01-10 01:48:37 +0800

    Add: BOW operation

--- bin/module/text/Preprocessor.py
+++ bin/module/text/Preprocessor.py
@@ -1,12 +1,12 @@
 import os
 import pickle
 import operator
-import nltk
-import gensim
 import pandas as pd
 import numpy as np
 from typing import List
+from gensim.models import KeyedVectors
 from nltk.probability import FreqDist
+from nltk import word_tokenize, sent_tokenize
 
 import bin.module.util as util
 from bin.setting import path, textPreprocessor as config
@@ -21,50 +21,25 @@ logger = util.general.initLogger(loggerName='TextPreprocessor')
 class Tokenizer():
 
     #2 articles with 2 sentences each
-    sampleAt = ['This is a test for text preprocessing. Do you think this could be a good way to expand your knowledge?', 'Is that because theres always an inherent overhead to using classes in Python? And if so, where does the overhead come from technically speaking.']
+    sampleAt = ['This is a test for text preprocessing. Do you think this could be a good way to expand your knowledge?', 'Is that because theres always an inherent overhead to using classes in Python? And if so, where does the overhead come from technically speaking.', ' ']
 
     def __init__(self, articles):
         self.articles = articles
         self.tokenized = []
     
     def tokenizeGen(self): #Generator only
-        assert util.general.isExhausted(self.articles), 'Articles are exhausted. Please re-initialize.'
+        self.articles = util.general.isExhausted(iter(self.articles))
+        assert self.articles , 'Articles are exhausted. Please re-initialize.'
 
-        tokenGen = ((nltk.word_tokenize(st) for st in nltk.sent_tokenize(at)) for at in self.articles)
+        tokenGen = ((word_tokenize(st) for st in sent_tokenize(at)) for at in self.articles)
         logger.info('Create token generator.')
         return tokenGen
     
     def tokenize(self): #Really produce the tokens
         if not self.tokenized:
-            assert util.general.isExhausted(self.articles), 'Articles are exhausted. Please re-initialize.'
-
             self.tokenized = util.general.createListFromGen(self.tokenizeGen())
             logger.info('Tokenized articles.')
         return self.tokenized
-
-    def brief(self):
-        """
-        Input: tokens with sentence structure.
-        """
-        self.tokenize()
-
-        #Word frequency distribution by nltk
-        fdist = FreqDist((tk for at in self.tokenized for st in at for tk in st))
-
-        nArticle = len(self.tokenized)
-        nSentence = sum(map(lambda article: len(article), self.tokenized))
-        nWord = fdist.N()
-
-        logger.info('About the corpus:')
-        logger.info('- Number of articles: ' + str(nArticle))
-        logger.info('- Number of sentences: ' + str(nSentence))
-        logger.info('- Number of terms: ' + str(nWord))
-        logger.info('- Number of unique terms: ' + str(fdist.B()))
-        logger.info('- Top terms:')
-        logger.info(sorted(fdist.items(), key=operator.itemgetter(1), reverse=True)[0:5])
-        logger.info('- Terms per sentence: ' + str(nWord / nSentence))
-        logger.info('- Terms per article: ' + str(nWord / nArticle))
-        logger.info('- Sentences per article: ' + str(nSentence / nArticle))
     
     @classmethod
     def test_tokenize(cls):
@@ -73,10 +48,6 @@ class Tokenizer():
         """
         print(cls.sampleAt)
         print(cls(cls.sampleAt).tokenize())
-    
-    @classmethod
-    def test_brief(cls):
-        cls(cls.sampleAt).brief()
 
 
 
@@ -85,6 +56,7 @@ class Tokenizer():
 class Normalizer():
     """
     Input: List of articles, with list of sentences -> [[[words], [words]]].
+    - It works faster than queued operation (in a list) implementation.
     """
 
     def __init__(self, tokens):
@@ -139,22 +111,71 @@ class Normalizer():
 
 
 
+class BOW():
+
+    def makeDict(self):
+        pass
+    
+    def makeHashDict(self):
+        pass
+
+    def export(self):
+        pass
+    
+    def brief(self):
+        """
+        Input: tokens with sentence structure.
+        """
+        self.tokenize()
+
+        #Word frequency distribution by nltk
+        fdist = FreqDist((tk for at in self.tokenized for st in at for tk in st))
+
+        nArticle = len(self.tokenized)
+        nSentence = sum(map(lambda article: len(article), self.tokenized))
+        nWord = fdist.N()
+
+        logger.info('About the corpus:')
+        logger.info('- Number of articles: ' + str(nArticle))
+        logger.info('- Number of sentences: ' + str(nSentence))
+        logger.info('- Number of terms: ' + str(nWord))
+        logger.info('- Number of unique terms: ' + str(fdist.B()))
+        logger.info('- Top terms:')
+        logger.info(sorted(fdist.items(), key=operator.itemgetter(1), reverse=True)[0:5])
+        logger.info('- Terms per sentence: ' + str(nWord / nSentence))
+        logger.info('- Terms per article: ' + str(nWord / nArticle))
+        logger.info('- Sentences per article: ' + str(nSentence / nArticle))
+
+    @classmethod
+    def test_brief(cls):
+        cls(cls.sampleAt).brief()
+
+
+
+
 #--Load experience keywords and embedding operations
 class EmbOperation():
-    
-    @staticmethod
-    def loadPretrainedEmb8Keywords(path):
-        """
-        Load pretrained emb at `path` and exp keywords
-        """
-        emb = gensim.models.KeyedVectors.load_word2vec_format(path, binary=True)
-        logger.info('Loaded pretrained embedding with', len(emb.index2word), 'words.')
 
+    @staticmethod
+    def loadKeywords(path):
+        """
+        Load experience keywords.
+        """
         keyWords = pd.read_csv(path.expKeyword, encoding='utf-8', header=None)[0].tolist()
         keyWords = [word.lower() for word in keyWords]
         logger.info('Load exp keywords of', len(keyWords), 'words.')
 
-        return (emb, keywords)
+        return keywords
+    
+    @staticmethod
+    def loadPretrainedEmb(path):
+        """
+        Load pretrained emb.
+        """
+        emb = KeyedVectors.load_word2vec_format(path, binary=True)
+        logger.info('Loaded pretrained embedding with' + str(len(emb.index2word)) + 'words.')
+
+        return emb
 
     @staticmethod
     def getSentenceEmb(at, emb):
@@ -163,7 +184,7 @@ class EmbOperation():
         - Return: [sent=array(300,)]
         """
         sentenceEmb = [np.array([emb[tk] for tk in st]).mean(axis=0) for st in at]
-        logger.info('Acquired sentence embedding of {} sentences.'.format(len(sentenceEmb)))
+        logger.info('Acquired sentence embedding of {} sentences.'.format(str(len(sentenceEmb))))
         return sentenceEmb
 
     @staticmethod
